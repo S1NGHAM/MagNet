fcc90e54fb198e4fa7d480161879c5c8c1b43cc6
Kevin Oliva Alva <kevin.oliva.alva@gmail.com>
2023-06-14 15:21:45 -0500

    Feat: study created

---
+++ src/main/java/com/mag/magnet/MagNetApplication.java
@@ -0,0 +1,13 @@
+package com.mag.magnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mag/magnet/MagNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mag.magnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
